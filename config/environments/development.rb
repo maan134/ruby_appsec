0c9dafbae011c237e72418f6f0b266a75c6a1c17
maan134 <135526000+maan134@users.noreply.github.com>
2026-01-10 15:53:14 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.hosts = ["localhost", "ruby_appsec-web-1"]
+  config.hosts = ["localhost", "ruby_appsec_web_1"]
   config.cache_classes = false
 
   # Allow Rails console from Docker network (ZAP)
